f4a5e87f5e085e44ee33c1eaacfb0e4c83e0e105
inddiara <indiara96d@gmail.com>
2017-12-06 01:07:57 -0200

    Function build list

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,12 +1,24 @@
 // Create out image list component
 // Import React
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400'},
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
+  { title: 'Mug', link: 'https://dummyimage.com/600x400' }
+];
+
 
 // Create component
 const ImageList = () => {
+  const RenderedImages = IMAGES.map(function (image) {
+    return <ImageDetail />
+  });
+
   return (
     <ul>
-      <li>Image Detail</li>
+      {RenderedImages}
     </ul>
   );
 };
